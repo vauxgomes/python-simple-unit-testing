79eb4be5879ac0f54f0048707d2fd848135b7164
Vaux Gomes <vauxsandino@gmail.com>
2018-11-12 21:32:14 -0300

    Adding some float tests

--- test_calc.py
+++ test_calc.py
@@ -25,6 +25,9 @@ class TestMath(unittest.TestCase):
 		self.assertEqual(calc.add(-1,-1), -2)
 		self.assertEqual(calc.add(-1,0), -1)
 
+		# Float test
+		self.assertEqual(calc.add(-1,2.0), 1.0)
+
 	#
 	def test_sub(self):
 		"""Testing add function"""
@@ -32,6 +35,9 @@ class TestMath(unittest.TestCase):
 		self.assertEqual(calc.sub(1,-1), 2)
 		self.assertEqual(calc.sub(-1,-1), 0)
 		self.assertEqual(calc.sub(-1,0), -1)
+
+		# Float test
+		self.assertEqual(calc.sub(-1,2.0), -3.0)
 
 	#
 	def test_mul(self):
@@ -41,14 +47,21 @@ class TestMath(unittest.TestCase):
 		self.assertEqual(calc.mul(-1,-1), 1)
 		self.assertEqual(calc.mul(-1,0), 0)
 
+		# Float test
+		self.assertEqual(calc.mul(1.0,2.0), 2.0)
+
 
 	def test_div(self):
 		"""Testing add function"""
 		self.assertEqual(calc.div(1,1), 1)
 		self.assertEqual(calc.div(1,-1), -1)
 		self.assertEqual(calc.div(-1,-1), 1)
-		
-		#
+		# self.assertEqual(calc.div(7,2), 3) # Only in Python 2
+
+		# Float test
+		self.assertEqual(calc.div(7,2.0), 3.5)
+
+		# Exception test
 		with self.assertRaises(ValueError):
 			calc.div(1, 0)
 
